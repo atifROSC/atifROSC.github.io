c5adde1b4e7a247878e52d920650921c26dad2be
atifROSC <98179220+atifROSC@users.noreply.github.com>
2022-05-31 11:15:02 -0400

    Update overview.js

--- overview.js
+++ overview.js
@@ -1,1 +1,25 @@
 
+`use strict`;
+
+const btnTrauma = document.querySelector(`.trauma-info-card`);
+const traumaModal = document.querySelector(`.trauma-center-modal`);
+const allOverviewModals = document.querySelectorAll(`.about-us-modal`);
+
+const openTraumaModal = () => {
+  traumaModal.classList.add(`visible`);
+  openModal();
+};
+
+const closeOverviewModal = () => {
+  for (let each of allOverviewModals) {
+    each.classList.remove("visible");
+  }
+  overlay.classList.remove("visible");
+  btnClose.classList.remove("visible");
+  body.style.overflowY = "scroll";
+  body.style.overflow = "overlay";
+};
+
+btnTrauma.addEventListener(`click`, openTraumaModal);
+overlay.addEventListener("click", closeOverviewModal);
+btnClose.addEventListener("click", closeOverviewModal);
